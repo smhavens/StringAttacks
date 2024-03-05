29442b91babe24abef3e9278ca851d9f8564ec6f
smhavens <smhavens@umich.edu>
2024-03-05 15:28:13 -0500

    Base weaknesses

--- main.c
+++ main.c
@@ -6,10 +6,44 @@
 // and finally dangling pointers.
 
 #include <stdio.h>
+#include "main.h"
 
+#define BUFSIZE 256
 
 int main(int argc, char** argv) {
 
-    //Poor formatting, user can insert commands.
+    // Poor formatting, user can insert commands.
     print(argv[1]);
+
+    // Stack Buffer Overflow
+    stack_weakness(argv[1]);
+
+    // Heap Buffer Overflow
+    heap_weakness();
+
+    // Integer Vulnerabilities
+    integer_weakness();
+
+    // Dangling Pointers
+    dangling_pointer();
+}
+
+// Using examples from https://cwe.mitre.org/data/definitions/121.html
+int stack_weakness(char* vulnerability) {
+    char buf[BUFSIZE];
+    strcpy(buf, vulnerability);
+    print(buf);
+    return 0;
+}
+
+int heap_weakness() {
+    return 0;
+}
+
+int integer_weakness() {
+    return 0;
+}
+
+int dangling_pointer() {
+    return 0;
 }
--- main.h
+++ main.h
@@ -2,5 +2,16 @@
 #define _MAIN_H_
 
 
+void print_weakness();
+
+int stack_weakness(char* vulnerability);
+
+int heap_weakness();
+
+int integer_weakness();
+
+int dangling_pointer();
+
+
 
 #endif
