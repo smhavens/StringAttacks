c1926c885ac43c56308797cf67f92e4dc97a1bd1
smhavens <smhavens@umich.edu>
2024-03-05 15:04:56 -0500

    Basic example

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 // Main file with poorly implemented code leaving room for attacks on the OS.
+// Included attacks:
+// poor string formatting
+// stack and heap buffer overflows
+// integer vulnerabilities
+// and finally dangling pointers.
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+
+    //Poor formatting, user can insert commands.
+    print(argv[1]);
+}
